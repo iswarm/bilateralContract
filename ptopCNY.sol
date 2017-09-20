2b5a9c6ddf87f5d4be542b7958d0a5e87909aac8
iswarm <seasybuy@foxmail.com>
2017-09-20 09:02:00 +0800

    startPtopDeposit

--- ptopCNY.sol
+++ ptopCNY.sol
@@ -1,23 +1,50 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.15;
 
 contract PtopFiatCurrencies {
 
     struct Signers {
-        address ownerOne;
-        address ownerTwo;
-        bool signedOne;
-        bool signedTwo;
+        address aliceBank;
+        address bobCustomer;
+        bool signedBank;
+        bool signedCustomer;
+    }
+
+    struct PledgeStatus {
+        uint256 cashPledge;
+        bool locked;
     }
 
     mapping(bytes32 => Signers) signRecord; // 记录一个函数调用的签名双方及签名情况
+    mapping(address => PledgeStatus) cashPledge; // 记录aliceBank的押金
 
-    function startPtopDeposit(address _Bob, bytes32 _hash) return (bool) {}
+    event StartDeposit(address _aliceBank, address _bobCustomer, bytes32 _hash);
 
-    function endPtopDeposit(address _Bob, bytes32 _hash) return (bool) {}
+    function startPtopDeposit(address _party, bytes32 _hash) returns (bool) {
+        require(msg.sender!=_party);
+        if (!signRecord[_hash].signedBank) {
+            signRecord[_hash].aliceBank = msg.sender;
+            signRecord[_hash].bobCustomer = _party;
+            signRecord[_hash].signedBank = true;
+            return true;
+        } else if ( (msg.sender == signRecord[_hash].bobCustomer) && (signRecord[_hash].aliceBank == _party) && (!signRecord[_hash].signedCustomer) ) {
+            signRecord[_hash].signedCustomer = true;
+        }
+        
+        require(cashPledge[signRecord[_hash].aliceBank].cashPledge>0);
 
-    function withdrawPledge() return (bool) {}
+        cashPledge[signRecord[_hash].aliceBank].locked = true;
 
-    function askArbitrator(address _Alex) return (bool) {}
+        StartDeposit(signRecord[_hash].aliceBank,signRecord[_hash].bobCustomer,_hash);
 
-    function arbitrate(address _Bob, address _Alice, bytes32 _hash) return (bool) {}
+        return true;
+
+    }
+
+    function endPtopDeposit(address _party, bytes32 _hash) returns (bool) {}
+
+    function withdrawPledge() returns (bool) {}
+
+    function askArbitrator(address _alex) returns (bool) {}
+
+    function arbitrate(address _bob, address _alice, bytes32 _hash) returns (bool) {}
 }
