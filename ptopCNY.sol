51004bcd2189fdd4d32a2cb52b013c1e9b351aaa
iswarm <seasybuy@foxmail.com>
2017-09-20 21:24:39 +0800

    askArbitrator function

--- ptopCNY.sol
+++ ptopCNY.sol
@@ -7,6 +7,11 @@ contract PtopFiatCurrencies {
         address bobCustomer;
         bool[2] signedBank;
         bool[2] signedCustomer;
+        uint256 blockNumForTransfer;
+        uint256 blockNumForAskAbitrator;
+        uint256 startBlock;
+        address arbitrator;
+        bool arbitrateResult; // true: bobCustomer win; false 
     }
 
     struct PledgeStatus {
@@ -19,8 +24,9 @@ contract PtopFiatCurrencies {
 
     event StartDeposit(address _aliceBank, address _bobCustomer, bytes32 _hash);
     event EndDeposit(address _aliceBank, address _bobCustomer, bytes32 _hash);
+    event AskArbitrator(address _arbitrator, bytes32 _hash);
 
-    function startPtopDeposit(address _party, bytes32 _hash) returns (bool) {
+    function startPtopDeposit(address _party, bytes32 _hash, uint256 _blockNumForTransfer, uint256 _blockNumForAskAbitrator) returns (bool) {
         require(msg.sender!=_party);
         require(cashPledge[signRecord[_hash].aliceBank].cashPledge>10**18 && !cashPledge[signRecord[_hash].aliceBank].locked); // 检查数字资产持有方的ETH余额大于零且没有作为押金
         
@@ -37,6 +43,9 @@ contract PtopFiatCurrencies {
             return false;
         }     
         cashPledge[signRecord[_hash].aliceBank].locked = true; // 数字资产持有方的ETH用做押金：只能在一次交易中使用
+        signRecord[_hash].startBlock = block.number;
+        signRecord[_hash].blockNumForTransfer = _blockNumForTransfer; // 转账所需时间（转换为区块数，出一个块的时间平均14s。）
+        signRecord[_hash].blockNumForAskAbitrator = _blockNumForAskAbitrator; // 可以请求仲裁的时间（转换为区块数）
 
         StartDeposit(signRecord[_hash].aliceBank,signRecord[_hash].bobCustomer,_hash);
 
@@ -72,11 +81,20 @@ contract PtopFiatCurrencies {
     function withdrawPledge() returns (bool) {
         require(cashPledge[msg.sender].cashPledge>0);
         require(!cashPledge[msg.sender].locked);
+        // require(block.number>)
         msg.sender.transfer(cashPledge[msg.sender].cashPledge);
         return true;
     }
 
-    function askArbitrator(address _alex) returns (bool) {}
+    function askArbitrator(address _arbitrator, bytes32 _hash) returns (bool) {
+        require(block.number >= signRecord[_hash].startBlock + signRecord[_hash].blockNumForTransfer); // 检查进入仲裁请求时间段，但还没结束
+        require(block.number <= signRecord[_hash].blockNumForAskAbitrator + signRecord[_hash].startBlock + signRecord[_hash].blockNumForTransfer);
+        require(cashPledge[_arbitrator].cashPledge>0);
+        require(!cashPledge[_arbitrator].locked);
+        signRecord[_hash].arbitrator = _arbitrator;
+        AskArbitrator(_arbitrator,_hash);
+        return true;
+    }
 
     function arbitrate(address _bob, address _alice, bytes32 _hash) returns (bool) {}
 
